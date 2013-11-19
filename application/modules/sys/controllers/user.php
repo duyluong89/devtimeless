85e904a640463c56435d9c2fb61b7a5b14f85cf0
trinh duy luong <duyluongnd@gmail.com>
2013-11-20 01:00:43 +0700

    add admin themes

---
+++ application/modules/sys/controllers/user.php
@@ -0,0 +1,17 @@
+<?php
+class user extends SYS_Controller{
+	function __construct(){
+		parent::__construct();
+		$this->breadcrumb->append_crumb('User', create_url('sys/user'));
+	}
+	
+	function index(){
+		if ($this->input->is_ajax_request()) {
+			die('s');
+			$this->_process();
+			return;
+		}
+		$this->view = "user/list";
+		
+	}
+}
